ef9806118d1be2ebc90d24e4a8a53bd803d0e338
Brock Ellis <brock.ellis@agapered.com>
2015-12-19 23:46:58 -0600

    First commit

---
+++ Vagrantfile
@@ -0,0 +1,24 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure(2) do |config|
+  # Uses "ubuntu/trusty64" Ubuntu Server 14.04 LTS (Trusty Tahr) 64 bits
+  # If you need 32 bits change this line by:
+  #     config.vm.box = "ubuntu/trusty32"
+  config.vm.box = "ubuntu/trusty64"
+
+  # Forward the Rails server default port to the host.
+  # config.vm.network "forwarded_port", guest: 80, host: 8080
+  config.vm.network "forwarded_port", guest: 3000, host: 3000
+
+  # Provider-specific configuration.
+  config.vm.provider "virtualbox" do |vb|
+    # Customize the amount of memory on the VM (set to 1GB):
+    vb.memory = "1024"
+  end
+
+  config.vm.synced_folder "../railsbox-app", "/vagrant"
+  
+  # Enable provisioning with a shell script. Install dev stack.
+  config.vm.provision :shell, privileged: false, path: "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+echo "Update packages"
+sudo apt-get update
+
+echo "Build packages and requirements"
+sudo apt-get -y install make build-essential libssl-dev libreadline-dev zlib1g-dev
+
+echo "Prepare git enviornment"
+sudo apt-get install libcurl4-gnutls-dev libexpat1-dev gettext unzip
+
+echo "Installing Git"
+sudo apt-get -y install git
+
+echo "Configure Git"
+git config --global user.name "Brock Ellis"
+git config --global user.email "brock@sharproot.com"
+
+echo "NodeJS"
+sudo apt-get -y install nodejs
+
+echo "Perl locale warning fix"
+sudo locale-gen es_ES.UTF-8
+sudo dpkg-reconfigure locales
+
+## echo "Installing Apache"
+## sudo apt-get -y install apache2
+
+echo "MyySQL Server"
+# Credentials:
+#   User: root
+#   Password: root
+# If you want a different password, change next two lines.
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+sudo apt-get -y install mysql-server
+sudo apt-get -y install libmysqlclient-dev
+
+echo "Create test database"
+mysql -r root -uroot
+CREATE DATABASE html_development;
+echo "exit"
+
+echo "Rbenv"
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+
+echo "Ruby-build (rbenv install)"
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bashrc
+
+echo "Install Ruby 2.2.3"
+sudo -H -u vagrant bash -i -c 'rbenv install 2.2.3'
+
+echo "Set ruby global version"
+sudo -H -u vagrant bash -i -c 'rbenv global 2.2.3'
+
+echo "Install Gems: bundler and rails"
+sudo -H -u vagrant bash -i -c 'gem install bundler --no-ri --no-rdoc'
+sudo -H -u vagrant bash -i -c 'gem install rails'
+sudo -H -u vagrant bash -i -c 'rbenv rehash'
